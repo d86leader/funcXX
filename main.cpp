acf54c3f8a79db904d6961703c011e4ab6d9bbfb
d86leader <d86leader@gmail.com>
2018-04-13 19:12:38 +0700

    stylistic improvements

--- main.cpp
+++ main.cpp
@@ -1,3 +1,5 @@
+/* демоны, во что вы превратили мой си с классами */
+
 #include <iostream>
 #include "func_stuff.cpp"
 #include "list.cpp"
@@ -71,7 +73,6 @@ func main () -> int
 {
 	list<unsigned char> pre_start {70, 142, 2, 31, 115, 151, 148, 26, 202, 135, 121, 94, 28, 12, 103, 131, 42, 35, 170, 120, 44, 114, 28, 146, 142, 188, 200, 124, 203};
 	list<int> minu5 {181, 433, 343, 141, 291, 385, 281, 382, 458, 240, 465, 415, 278, 242, 232, 407, 432, 392, 199, 463, 434, 238, 168, 265, 430, 236, 377, 349, 408};
-
 	list<int> minu4 {151, 665, 436, 280, 339, 330, 320, 464, 622, 430, 664, 616, 194, 268, 511, 427, 472, 420, 459, 686, 475, 9, 232, 215, 499, 281, 588, 348, 584};
 	list<int> minu3 {352, 538, 405, 355, 565, 151, 556, 425, 360, 251, 525, 522, 235, 548, 454, 247, 119, 310, 475, 597, 203, 97, 159, 288, 285, 529, 587, 208, 469};
 	list<int> minu2 {370, 259, 344, 295, 420, 435, 267, 289, 359, 380, 435, 231, 433, 422, 325, 204, 307, 455, 325, 419, 391, 263, 433, 262, 220, 398, 422, 242, 337};
@@ -81,17 +82,19 @@ func main () -> int
 	let step = curry(fnc(map<int, unsigned char>))(operation);
 	let super_inter = curry(inter);
 
-	let conveyor = step *= super_inter(minu5)
+	let conveyor =
+		   step *= super_inter(minu5)
 		*= step *= super_inter(minu4)
 		*= step *= super_inter(minu3)
 		*= step *= super_inter(minu2)
 		*= step *= super_inter(minu1);
-
 	let end = conveyor(pre_start);
+
 	for (let i : end)
 	{
 		cout << i;
 	}
 	cout<<endl;
+
 	return 0;
 }
